7c1d006586c8472ced97ad11d9b9574301e9e724
EmaanSwati001 <u2024156@giki.edu.pk>
2026-01-13 20:51:21 +0500

    h

---
+++ 01basic/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Hello React!</h1>
+      <p>Counter: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </div>
+  );
+}
+
+export default App;
